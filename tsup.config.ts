17f4afb254dfaa9474e1461bdd0345e6d895eacf
juanignaciomolina <juanignaciomln@gmail.com>
2023-04-20 21:23:48 +0200

    Modified build and dev scripts. Changed tsup config to ditch the lib folder.

--- tsup.config.ts
+++ tsup.config.ts
@@ -14,6 +14,6 @@ export const tsup: Options = {
   entryPoints: ["src/index.ts"],
   watch: env === "development",
   target: "es2020",
-  outDir: env === "production" ? "dist" : "lib",
+  outDir: "dist",
   entry: ["src/**/*.ts"],
 }
