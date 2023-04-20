d76c39ae7a561d14ec93b540d570fc331c0d150a
juanignaciomolina <juanignaciomln@gmail.com>
2023-04-20 21:30:34 +0200

    Trying out a new tsupconfig

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,14 +3,14 @@ import type { Options } from "tsup"
 const env = process.env.NODE_ENV
 
 export const tsup: Options = {
-  splitting: true,
+  //splitting: true,
   sourcemap: env === "prod", // source map is only available in prod
   clean: true, // rimraf disr
   dts: true, // generate dts file for main module
   format: ["cjs", "esm"], // generate cjs and esm files
-  minify: env === "production",
-  bundle: env === "production",
-  skipNodeModulesBundle: true,
+  //minify: env === "production",
+  //bundle: env === "production",
+  //skipNodeModulesBundle: true,
   entryPoints: ["src/index.ts"],
   watch: env === "development",
   target: "es2020",
