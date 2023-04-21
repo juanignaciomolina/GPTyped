88f14111e5cff79ed815c1a8c3eefe64525ccdb9
juanignaciomolina <juanignaciomln@gmail.com>
2023-04-22 01:08:55 +0200

    Added docs website. Added Introduction and Quick start. Added easter egg to src/index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+/*
+   _____ _____ _______                   _ 
+  / ____|  __ \__   __|                 | |
+ | |  __| |__) | | |_   _ _ __   ___  __| |
+ | | |_ |  ___/  | | | | | '_ \ / _ \/ _` |
+ | |__| | |      | | |_| | |_) |  __/ (_| |
+  \_____|_|      |_|\__, | .__/ \___|\__,_|
+                     __/ | |               
+                    |___/|_|               
+
+
+Source: https://github.com/juanignaciomolina/GPTyped
+*/
+
 // Entry point for the package
 export * from "./clients"
 export * from "./prompters"
