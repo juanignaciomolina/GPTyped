a19af0c846a320f716347fba008f96d40e8b2c30
Juan Ignacio <juanignaciomln@gmail.com>
2023-04-22 04:19:27 +0200

    Documentation Website 🎉
    * Added docs website. Added Introduction and Quick start. Added easter egg to src/index
    * Implemented Clients sections: About and OpenAI
    * Removed custom components from the docs
    * Added doc for about-prompters
    * Documented all the methods from PrompterForObject
    * Added links between docs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+/*
+   _____ _____ _______                   _ 
+  / ____|  __ \__   __|                 | |
+ | |  __| |__) | | |_   _ _ __   ___  __| |
+ | | |_ |  ___/  | | | | | '_ \ / _ \/ _` |
+ | |__| | |      | | |_| | |_) |  __/ (_| |
+  \_____|_|      |_|\__, | .__/ \___|\__,_|
+                     __/ | |               
+                    |___/|_|               
+
+
+Learn more about this package at https://gptyped.com
+
+Source code: https://github.com/juanignaciomolina/GPTyped
+*/
+
 // Entry point for the package
 export * from "./clients"
 export * from "./prompters"
