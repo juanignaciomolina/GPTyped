ab0b56f5e6f115d9f89cfd70d416a9d938f6e3a0
Juan Ignacio <juanignaciomln@gmail.com>
2023-04-23 02:57:51 +0200

    Updated docs website OG configuration.

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { DocsThemeConfig } from "nextra-theme-docs"
 import { useConfig } from "nextra-theme-docs"
+import { useRouter } from "next/router"
 
 const config: DocsThemeConfig = {
   logo: (
@@ -65,6 +66,25 @@ const config: DocsThemeConfig = {
         },
       ],
     }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url = "https://gptyped.com" + (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || "GPtyped"} />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description ||
+            "A simple library to send, receive and verify structured data (like JSON) to OpenAI and other LLMs."
+          }
+        />
+      </>
+    )
   },
   footer: {
     text: (
